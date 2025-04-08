6e10570ede7edbf85e338e1aec5aa1d9b97ebddb
Afecom <88211665+Afecom@users.noreply.github.com>
2025-04-08 18:56:43 +0000

    Tue, Apr 8, 2025, 9:56 PM +03:00

--- cash/cash.c
+++ cash/cash.c
@@ -1,5 +1,5 @@
 #include<cs50.h>
-#include<stdio.j>
+#include<stdio.h>
 
 int calculate_quarters(int cents);
 int calculate_dimes(int cents);
@@ -11,20 +11,20 @@ int main(void)
     int cents;
     do
     {
-        int cents = get_int("change owed: ");
+        cents = get_int("change owed: ");
     }
     while (cents < 0);
 }
 
-int calculate_quarters(int cents)
-{}
+//int calculate_quarters(int cents)
+//{}
 
-int calculate_dimes(int cents)
-{}
+//int calculate_dimes(int cents)
+//{}
 
-int calculate_nickels(int cents)
-{}
+//int calculate_nickels(int cents)
+//{}
 
-int calculate_pennies(int cents)
-{}
+//int calculate_pennies(int cents)
+//{}
 
