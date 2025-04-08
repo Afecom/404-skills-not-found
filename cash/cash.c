8a26dcf00859301ba5948ecfa5654a65a847cf73
Afecom <88211665+Afecom@users.noreply.github.com>
2025-04-08 18:54:50 +0000

    Tue, Apr 8, 2025, 9:54 PM +03:00

---
+++ cash/cash.c
@@ -0,0 +1,30 @@
+#include<cs50.h>
+#include<stdio.j>
+
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
+
+int main(void)
+{
+    int cents;
+    do
+    {
+        int cents = get_int("change owed: ");
+    }
+    while (cents < 0);
+}
+
+int calculate_quarters(int cents)
+{}
+
+int calculate_dimes(int cents)
+{}
+
+int calculate_nickels(int cents)
+{}
+
+int calculate_pennies(int cents)
+{}
+
