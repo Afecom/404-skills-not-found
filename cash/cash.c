79007a9e3587afd00cd43b3bd46f1d6564a7ae77
Afecom <88211665+Afecom@users.noreply.github.com>
2025-04-09 03:10:04 +0000

    Wed, Apr 9, 2025, 6:10 AM +03:00

--- cash/cash.c
+++ cash/cash.c
@@ -1,7 +1,7 @@
 #include<cs50.h>
 #include<stdio.h>
 
-int calculation(int cents);
+void calculation(int cents);
 
 int main(void)
 {
@@ -15,43 +15,17 @@ int main(void)
     calculation(cents);
 }
 
-int calculation(int cents)
+void calculation(int cents)
 {
     int quarters = 0;
+    int dimes = 0;
+    int nickels = 0;
+    int pennies = 0;
+
     while (cents >= 25)
     {
         quarters++;
-        cents -= 25;
     }
-    return quarters;
     printf("%i\n", quarters);
-
-    int dimes = 0;
-    while (cents >= 10)
-    {
-        dimes++;
-        cents -= 10;
-    }
-    return dimes;
-
-    int nickels = 0;
-    while (cents >= 5)
-    {
-        nickels++;
-        cents -= 5;
-    }
-    return nickels;
-
-    int pennies = 0;
-    while (cents >= 1)
-    {
-        pennies++;
-        cents--;
-    }
-    return pennies;
-
-    int sum = quarters + dimes + nickels + pennies;
-
-    printf("%i\n", sum);
 }
 
