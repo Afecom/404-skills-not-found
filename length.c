e0f451efe09a4e208115d25cb478c0fd00614140
Afecom <88211665+Afecom@users.noreply.github.com>
2025-04-11 16:03:26 +0000

    Fri, Apr 11, 2025, 7:03 PM +03:00

--- length.c
+++ length.c
@@ -4,6 +4,13 @@
 
 int main(void)
 {
-    string text = get_string("Please provide your name to be counted: ");
-    
+    string text = get_string("Please provide your name: ");
+
+    int n = 0;
+    while (text[n] != '\0')
+    {
+        printf("%c", text[n]);
+        n++;
+    }
+    printf("\n");
 }
