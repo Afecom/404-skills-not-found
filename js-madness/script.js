9a67910a5dec6734fc3db1c5a1555d6a3b27a7b9
Nuru <knuru472@gmail.com>
2025-06-13 17:04:30 +0300

    Added some JS-madness

---
+++ js-madness/script.js
@@ -0,0 +1,22 @@
+// My First Javascript code FR :)
+console.log('Hello World!');
+
+// Getting more things lets see
+let firstName = 'Nuru';
+let lastName = 'Kamil';
+console.log('Hello:) this is', firstName, lastName,'! A mad dev :)'); // Gosh it's working :) :)
+
+let nuru = [];
+nuru[0] = 'Nure';
+nuru[1] = 'Kamil.';
+nuru[2] = 21;
+
+function introduction(FirstName, LastName, Age){
+    console.log('BTW my official ID card name is', FirstName,  LastName,'And my age as of now (06/13/2025) is', Age,'!');
+}
+
+function ageCalculator(){
+    return 10 + 11;
+}
+
+introduction(nuru[0], nuru[1], ageCalculator())
